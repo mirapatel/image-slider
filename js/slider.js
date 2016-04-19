c498b85ac2e03896091ca70cab95c32eb2f370f9
Miral Patel <miralpatel1506@hotmail.com>
2016-04-20 09:44:35 +1200

    show image in slider with buttons

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,50 @@
 // alert("hello");
+
+//get all the links of the images
+var allImages = document.querySelectorAll ("#gallery img");
+
+// console.log(allImages);
+
+var imageSources = [];
+
+for (var i = 0; i<allImages.length; i++) {
+	imageSources.push(allImages[i].src); 
+}
+
+// console.log(imageSources);
+
+//delete all the images on the page
+var galleryContainer = document.querySelector("#gallery");
+
+galleryContainer.innerHTML = "";
+
+//create a slider container
+var slider = document.createElement ("div");
+
+//attach an ID to the container
+slider.id = "slider";
+
+//add the slider to the browser
+galleryContainer.appendChild(slider);
+
+//create come buttons
+var next = document.createElement("button");
+var prev = document.createElement("button");
+
+next.id = "next";
+prev.id = "prev";
+
+//add text to the buttons
+next.innerHTML = "Next";
+prev.innerHTML = "Previous";
+
+//add buttons to the slider
+slider.appendChild(next);
+slider.appendChild(prev);
+
+//show the first image to the slider
+slider.style.backgroundImage = "url("+ imageSources[0] + ")";
+
+
+
+
